27918ca6511cf87e9a65eed180fbeaa9521daf67
Shivam Patel <shivamp3107@gmail.com>
2026-09-02 13:51:38 -0400

    Added code to solve lab

--- CGT-215-Lab-02-Pate2768/CGT-215-Lab-02-Pate2768.cpp
+++ CGT-215-Lab-02-Pate2768/CGT-215-Lab-02-Pate2768.cpp
@@ -1,20 +1,23 @@
-// CGT-215-Lab-02-Pate2768.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Hello my name is Shivam and I will solve this equation: "
+		<< endl
+		<< "Ax + B = 0"
+		<< endl 
+		<<"For x"
+		<< endl;
+	float A;
+	float B;
+	float X;
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	X = (0 - B);
+	X = X / A;
+	cout << "Solving " << A << "x + " << B << " = 0" << endl;
+	cout << "The answer is: " << endl;
+	cout << "x = " << X << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
